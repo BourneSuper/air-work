55f224facd53301600263b8d07609462276c0c1d
BourneSuper <BourneSuper@github.com>
2020-06-17 18:26:17 +0800

    update index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Air Work
                 </div>
 
                 <div class="links">
@@ -92,7 +92,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/BourneSuper/air-work">GitHub</a>
                 </div>
             </div>
         </div>
