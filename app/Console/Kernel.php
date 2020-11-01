05ccb3d65eba5c6659c3b031a483592f4f62da05
BourneSuper <BourneSuper@github.com>
2020-11-01 22:14:33 +0800

    优化

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule) {
         // $schedule->command('inspire')->hourly();
         
-        $schedule->command( 'maintain:freshTablesMonthly' )->cron('* * 1 * *')->withoutOverlapping();
+        $schedule->command( 'maintain:freshTablesMonthly' )->cron('1 0 1 * *')->withoutOverlapping();
     }
 
     /**
